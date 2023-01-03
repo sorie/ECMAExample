cecb5b071ba6ff8603be5e6b8caaaa2548003d29
lala <enne3939@gmail.com>
2023-01-03 14:37:35 +0900

    Create createElement.js

---
+++ example/createElement.js
@@ -0,0 +1,28 @@
+function createH1(props) {
+  return [document.createElement('h1')]
+  .map(element => {
+    Object
+    .entries{ ...props, 'data-id': 'subject' })
+    .forEach({[name, value]) => element.setAttribute(name, value))
+    return element;
+  })[0];
+}
+
+function createDiv(props) {
+  return [document.createElement('div')]
+  .map(element => {
+    Object
+    .entries({ ...props, 'data-id': 'layout' })
+    .forEach({[name, value]) => element.setAttribute(name, value))
+    return element;
+  })[0];
+}
+
+const createMap = {
+  h1: createH1,
+  div: createDiv,
+};
+
+function createElementtype, props) {
+  return createMap[type](props);
+}
